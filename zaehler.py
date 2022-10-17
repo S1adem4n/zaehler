c2c23c0a3cb5722a28787bf8291b8577118a6034
Jonathan <jonathanbuko@icloud.com>
2022-10-17 21:24:19 +0200

    improve efficiency

--- zaehler.py
+++ zaehler.py
@@ -1,29 +1,30 @@
 import time
+import os
 from datetime import datetime
 import pandas as pd
 import RPi.GPIO as GPIO
 
 myGPIO = 18
-count = 0
+last_count = float(os.popen("tail -n 1 %s" % "zaehlerstand.csv").read().split("\t")[0])
 impulse = False
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(myGPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+
 try:
-        while True:
-                if GPIO.input(myGPIO):
-                        impulse = True
-                        time.sleep(0.5)
-                if impulse:
-                        df = pd.read_csv("zaehlerstand.csv", delimiter="\t", names=["Zählerstand", "Datum"])
-                        last_count = df.iloc[-1, 0]
+    while True:
+        if GPIO.input(myGPIO):
+            impulse = True
+            time.sleep(0.5)
+        if impulse:
+            last_count = round(last_count+.001, 3)
+            with open("zaehlerstand.csv", "a") as file:
+                date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+                file.write(f"{last_count}\t{date}\n")
+            
+            impulse = False
 
-                        to_add = pd.DataFrame({"Zählerstand": round(last_count+0.01, 2), "Datum": datetime.now().strftime("%Y-%m-%dT%H:%M:%S")}, index=[len(df)])
-                        df = pd.concat([df, to_add])
-
-                        df.to_csv("zaehlerstand.csv", sep="\t", header=False, index=False)
-                        impulse = False
-except:
-        print("fehler")
+except Exception as e:
+        print(f"Fehler: {e}")
         GPIO.cleanup()
