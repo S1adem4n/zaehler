2774c096baaa05603ce27553b36ef33bdf802714
Jonathan <jonathanbuko@icloud.com>
2022-10-21 13:00:49 +0200

    fix double counting

--- zaehler.py
+++ zaehler.py
@@ -6,7 +6,6 @@ import RPi.GPIO as GPIO
 
 myGPIO = 18
 last_count = float(os.popen("tail -n 1 %s" % "zaehlerstand.csv").read().split("\t")[0])
-impulse = False
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(myGPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -14,16 +13,15 @@ GPIO.setup(myGPIO, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 try:
     while True:
-        if GPIO.input(myGPIO):
-            impulse = True
-            time.sleep(0.5)
-        if impulse:
-            last_count = round(last_count+.001, 3)
+        if GPIO.input(myGPIO) == GPIO.HIGH:
+            while GPIO.input(myGPIO) == GPIO.HIGH:
+                time.sleep(0.1)
+            last_count = round(last_count+.01, 2)
             with open("zaehlerstand.csv", "a") as file:
                 date = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
                 file.write(f"{last_count}\t{date}\n")
+            time.sleep(5)
             
-            impulse = False
 
 except Exception as e:
         print(f"Fehler: {e}")
