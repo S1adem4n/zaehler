30ca7e693b8801e9ca5bd682a1b7ddb87bb4f1ed
Jonathan <jonathanbuko@icloud.com>
2022-10-22 12:37:06 +0200

    remove test

--- test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("zaehlerstand.csv", delimiter="\t", names=["Zählerstand", "Datum"])
-df["Verbrauch"] = round(df["Zählerstand"].diff(), 4)
-df['Datum'] = pd.to_datetime(df['Datum'], format="%Y-%m-%dT%H:%M:%S")
-
-df.loc[len(df)-1, ["Echter Zählerstand"]] = 7
-print(df)
