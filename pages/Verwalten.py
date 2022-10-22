a0190d814a6a8d014ec82588bed353f33e4be5e9
Jonathan <jonathanbuko@icloud.com>
2022-10-22 12:28:43 +0200

    add manage page

---
+++ pages/Verwalten.py
@@ -0,0 +1,29 @@
+import csv
+import datetime
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="Gaszähler: Testen",
+    page_icon="📁",
+    layout="wide",
+)
+
+st.title("Gaszähler")
+
+real_count_input = st.number_input("\"Echten\" Zählerstand eintragen")
+if st.button("In Datei eintragen"):
+    df = pd.read_csv("zaehlerstand.csv", delimiter="\t", names=["Zählerstand", "Datum"])
+    df["Verbrauch"] = round(df["Zählerstand"].diff(), 4)
+    df['Datum'] = pd.to_datetime(df['Datum'], format="%Y-%m-%dT%H:%M:%S")
+
+    df.loc[len(df)-1, ["Echter Zählerstand"]] = real_count_input
+    st.success("Erfolgreich hinzugefügt!")
+
+reset_count_input = st.number_input("Zählerstand zurücksetzen")
+if st.button("Zurücksetzen"):
+    with open("zaehlerstand.csv", "w+") as file:
+        writer = csv.writer(file, dialect="excel-tab")
+        time = datetime.datetime.now()
+        writer.writerow([reset_count_input, time.strftime("%Y-%m-%dT%H:%M:%S")])
+    st.success("Zählerstand erfolgreich zurückgesetzt!")
